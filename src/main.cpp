ddf5413f5b80e70380221563f310f3ae7a0f067f
alex-makes-things <th3redshad0w@gmail.com>
2025-06-09 00:50:00 +0200

    Added nearest-neighbor scaling
    Added a function for fast and reliable nearest-neighbor scaling, along with a simple test which scales "nicerlandscape" from 0.1 to 1 in the course of 5 seconds, mixing linear interpolation and nn, and pausing for 1 second. On loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <Adafruit_ST7735.h>
 #include <SPI.h>
 #include "nicerlandscape.h"
-#include "test.h"
+#include <scaler.h>
 
 #define SDA 21
 #define SCL 22
@@ -22,6 +22,7 @@ void fastRender(int16_t x, int16_t y, uint16_t *bitmap, int16_t w, int16_t h);
 float mapM(float x, float in_min, float in_max, float out_min, float out_max);
 void loadingBar(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t startTime, uint16_t duration, uint16_t color);
 int lerp(float v0, float v1, float t);
+float lerpF(float v0, float v1, float t);
 float clamp(float n, float min, float max);
 //-----------FUNCTION PROTOTYPES----------------//
 
@@ -35,12 +36,17 @@ void setup() {
 }
 
 //-------------BEFORE LOOP----------------//
-uint64_t start = millis();
+uint64_t start, lerptime = millis();
+uint64_t pauseT = 0;
+static bool isDone = false;
 //-------------BEFORE LOOP----------------//
 
 
 //-------------SETTINGS----------------//
 bool render_frametime = false;
+float init_scaling = 0.1;
+float scaling_factor = 0.1;
+float duration = 5000;
 //-------------SETTINGS----------------//
 
 
@@ -54,10 +60,28 @@ void loop() {
       canvas.setTextWrap(false);
       canvas.print(millis()-start);
     }
-    
-    
 
+    if(scaling_factor < 1 && isDone == false){
+      scaling_factor = lerpF(init_scaling, 1, mapM(millis()-lerptime, 0, duration, 0, 1));
+      Serial.println(scaling_factor);
+    }else if (isDone && millis()-pauseT>=1000){
+      isDone = false;
+      scaling_factor = init_scaling;
+      lerptime = millis();
+    }else if (scaling_factor>0.99 && isDone == false){
+      pauseT = millis();
+      scaling_factor = 1;
+      isDone = true;
+      Serial.println("Finished");
+    }
+
+    int arraysize = int(floor((NICERLANDSCAPE_WIDTH*scaling_factor)*(NICERLANDSCAPE_HEIGHT*scaling_factor)));
+    uint16_t* output = new uint16_t[arraysize];
+    pair<unsigned int, unsigned int> imagesize = scale(nicerlandscape, NICERLANDSCAPE_WIDTH, NICERLANDSCAPE_HEIGHT, output, scaling_factor);
+    canvas.drawRGBBitmap(0,0, output, imagesize.first, imagesize.second);
+    delete[] output;
     fastRender(0,0,canvas.getBuffer(),SCREENWIDTH,SCREENHEIGHT);
+    start = millis();
 }  
 
 
@@ -84,6 +108,9 @@ void loadingBar(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t startTi
 }
 int lerp(float v0, float v1, float t) {
   return int(round((1 - t) * v0 + t * v1));
+}
+float lerpF(float v0, float v1, float t) {
+  return (1 - t) * v0 + t * v1;
 }
 float clamp(float n, float min, float max) {
   return std::max(min, std::min(n, max));
---
+++ src/scaler.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+pair<unsigned int, unsigned int> scale(const uint16_t* input, int width, int height, uint16_t* result, float scaling_factor){
+    
+    unsigned int count = 0;
+    unsigned int newwidth = floor(width*scaling_factor);
+    unsigned int newheight = floor(height*scaling_factor);
+
+    for(unsigned int b=0; b < newheight; b++){
+        for(unsigned int i=0; i < newwidth; i++){
+            unsigned int scaled_h = floor(b/scaling_factor);
+            unsigned int scaled_w = floor(i/scaling_factor);
+            result[count] = input[(scaled_h * width) + scaled_w];
+            count++;
+        }
+    }
+
+    pair<unsigned int, unsigned int> newsize = {newwidth, newheight};
+    return newsize;
+}
+
+/*
+    float scaling_factor = 0.25;
+    pair<float,float> imsize = {128, 64};
+    int arraysize = int(floor((imsize.first*scaling_factor)*(imsize.second*scaling_factor)));
+    uint16_t* output = new uint16_t[arraysize];
+    //pair<unsigned int, unsigned int> imagesize = scale(nicerlandscape, 128, 64, output, scaling_factor);
+    delete[] output;
+*/
