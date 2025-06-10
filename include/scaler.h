47070ae6321f450eeaf45d502eda0dbe8b444b2d
alex-makes-things <th3redshad0w@gmail.com>
2025-06-10 16:02:22 +0200

    Used constants instead of mutable integers
    According to the ESP documentation, using constants whenever possible should reduce memory usage

--- include/scaler.h
+++ include/scaler.h
@@ -8,57 +8,50 @@ using namespace std;
 
 
 
-pair<unsigned int, unsigned int> scale(const uint16_t* input, int width, int height, uint16_t* result, float scaling_factor){
-    
+pair<unsigned const int, unsigned const int> scale(const uint16_t* input, int width, int height, uint16_t* result, float scaling_factor){
     unsigned int count = 0;
-    unsigned int newwidth = floor(width*scaling_factor);
-    unsigned int newheight = floor(height*scaling_factor);
-
+    unsigned const int newwidth = floor(width*scaling_factor);
+    unsigned const int newheight = floor(height*scaling_factor);
     for(unsigned int b=0; b < newheight; b++){
         for(unsigned int i=0; i < newwidth; i++){
-            unsigned int scaled_h = floor(b/scaling_factor);
-            unsigned int scaled_w = floor(i/scaling_factor);
+            unsigned const int scaled_h = floor(b/scaling_factor);
+            unsigned const int scaled_w = floor(i/scaling_factor);
             result[count] = input[(scaled_h * width) + scaled_w];
             count++;
         }
     }
-
-    pair<unsigned int, unsigned int> newsize = {newwidth, newheight};
+    pair<unsigned const int, unsigned const int> newsize = {newwidth, newheight};
     return newsize;
 }
 
-pair<unsigned int, unsigned int> scale(const uint8_t* input, int width, int height, uint8_t* result, float scaling_factor){
-    
-    unsigned int newwidth  = floor(width * scaling_factor);
-    unsigned int newheight = floor(height * scaling_factor);
-
-    int in_row_bytes  = (width + 7) / 8;
-    int out_row_bytes = (newwidth + 7) / 8;
-
+pair<unsigned const int, unsigned const int> scale(const uint8_t* input, int width, int height, uint8_t* result, float scaling_factor){
+    unsigned const int newwidth  = floor(width * scaling_factor);
+    unsigned const int newheight = floor(height * scaling_factor);
+    const int in_row_bytes  = (width + 7) / 8;
+    const int out_row_bytes = (newwidth + 7) / 8;
     // Clear the output buffer
-    int out_size = out_row_bytes * newheight;
+    const int out_size = out_row_bytes * newheight;
     for (int i = 0; i < out_size; i++) {
         result[i] = 0;
     }
-
     for (unsigned int y = 0; y < newheight; y++) {
         for (unsigned int x = 0; x < newwidth; x++) {
             // Map to source pixel using nearest neighbor
-            unsigned int src_y = floor(y / scaling_factor);
-            unsigned int src_x = floor(x / scaling_factor);
-
-            int src_byte_index = src_y * in_row_bytes + src_x / 8;
-            uint8_t src_mask = 0x80 >> (src_x % 8);
-            bool pixel_on = input[src_byte_index] & src_mask;
-
+            unsigned const int src_y = floor(y / scaling_factor);
+            unsigned const int src_x = floor(x / scaling_factor);
+            // Get the index of the byte containing the pixel we look for
+            const int src_byte_index = src_y * in_row_bytes + src_x / 8;
+            const uint8_t src_mask = 0x80 >> (src_x % 8);
+            const bool pixel_on = input[src_byte_index] & src_mask;  //See if pixel is on or off
+            // Set the output pixel at the scaled coordinates to the result of the previous check
             if (pixel_on) {
-                int dst_byte_index = y * out_row_bytes + x / 8;
-                uint8_t dst_mask = 0x80 >> (x % 8);
+                const int dst_byte_index = y * out_row_bytes + x / 8;
+                const uint8_t dst_mask = 0x80 >> (x % 8);
                 result[dst_byte_index] |= dst_mask;
             }
         }
     }
-    pair<unsigned int, unsigned int> newsize = {newwidth, newheight};
+    pair<unsigned const int, unsigned const int> newsize = {newwidth, newheight};
     return newsize;
 }
 
