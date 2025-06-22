0b3d34a8f9d958e2996bc831513016f8183e71d0
alex-makes-things <th3redshad0w@gmail.com>
2025-06-22 02:02:25 +0200

    First steps to a perfect focusing system
    The goal of this commit is completely overhauling the previous focusing system, now reaching into an extremely versatile algorithm which will support absolutely every kind of UI layout.
    Changelog:
    -Introduced a scene struct which contains the pointers to every UIElement present in the scene, organized by id in an unordered map.
    -Finally introduced UI class, which is meant to handle the rendering of elements and the focusing system.
    
    Future commits:
    -Finish the focusing system.
    -Extreme code organization overhaul (dividing files in .h and .cpp files)
    -Add new types of elements

--- include/UIAssist.h
+++ include/UIAssist.h
@@ -4,6 +4,14 @@
 #include <functional>
 #include <string>
 #include <map>
+#include <vector>
+#include <unordered_map>
+
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
 
 using std::string;
 
@@ -15,16 +23,21 @@ void renderBmp8(int x, int y, Image8 img, float scaling, uint16_t color);
 //-----------FUNCTION PROTOTYPES----------------//
 
 
+
 struct Coordinates{
   int x=0;
   int y=0;
   Coordinates(int posx=0, int posy=0):x(posx), y(posy){}
 };
 
+
 struct Identity{
   unsigned int scene_id=0;
   unsigned int ele_id=0;
+  Identity(unsigned int scene=0, unsigned int element=0) : scene_id(scene), ele_id(element){}
 };
+
+
 
 class Animator{
   private:
@@ -175,6 +188,7 @@ class UIElement{
     unsigned int width=0, height=0;
     Identity id;
     public:
+    Animator anim;
     bool centered = false;
     bool draw=true; //If true, the element is drawn, if false it's kept hidden.
     UIElement(unsigned int w=0, unsigned int h=0, unsigned int posx=0, unsigned int posy=0){
@@ -182,6 +196,7 @@ class UIElement{
         height = h;
         position = Coordinates(posx, posy);
     }
+    void InitAnim(float initial, float final, unsigned int duration){anim = Animator(initial, final, duration);}
     void setPosX(unsigned int X){position.x = X;}
     void setPosY(unsigned int Y){position.y = Y;}
     void setPos(unsigned int X, unsigned int Y){position.x=X;position.y=Y;}
@@ -191,13 +206,12 @@ class UIElement{
       id.scene_id = scene;
       id.ele_id = own;
     }
-    Identity getId(){
-      return id;
-    }
+    Identity getId(){return id;}
+    Coordinates getPos(){return position;}
+    unsigned int getWidth(){return width;}
+    unsigned int getHeight(){return height;}
     virtual void render() = 0;
-    void setCenter(bool center){
-      centered = center;
-    }
+    void setCenter(bool center){centered = center;}
     Coordinates centerPos(unsigned int x_pos, unsigned int y_pos, unsigned int w, unsigned int h){
       unsigned int new_x = floor((float)x_pos-((float)w/2));
       unsigned int new_y = floor((float)y_pos-((float)h/2));
@@ -212,11 +226,12 @@ class MonoImage : public UIElement{
     uint16_t color = 0xffff;
   public:
     bool overrideScaling = false;
-    Animator anim;
-    MonoImage(Image8 *img, unsigned int posx=0, unsigned int posy=0):UIElement(img->width, img->height, posx, posy){
+    
+    MonoImage(Image8 *img, unsigned int posx=0, unsigned int posy=0, unsigned int id=0):UIElement(img->width, img->height, posx, posy){
         body = img;
+        setID(id);
     }
-    void InitAnim(float initial, float final, unsigned int duration){anim = Animator(initial, final, duration);}
+
     void setScale(float scale){scale_fac = scale;}
     float getScale(){return scale_fac;}
     void setColor(uint16_t hue){color = hue;}
@@ -247,7 +262,34 @@ class MonoImage : public UIElement{
     }
 };
 
+struct Scene{
+  unsigned int id;
+  bool isActive;
+  std::unordered_map<unsigned int, UIElement*> elements;
 
+  Scene(std::vector<UIElement*> elementGroup = {}, unsigned int scene_id = 0, bool active = false){
+    for(int i = 0; i<elementGroup.size(); i++){
+      elements.insert({elementGroup[i]->getId().ele_id,elementGroup[i]});
+    }
+    id = scene_id;
+    isActive = active;
+    for(UIElement* elem : elementGroup){
+      elem->setSceneID(id);
+    }
+  }
+
+  int howManyElements(){
+    return elements.size();
+  }
+
+  void renderScene(){
+    if(isActive){
+      for(auto elem : elements){
+        elem.second->render();
+      }
+    }
+  }
+};
 
 struct Focus{
   Identity current;
@@ -277,16 +319,126 @@ struct Focus{
 };
 
 
-bool areStill(std::vector<MonoImage*> images){
-  int count = 0;
-  for (MonoImage* image : images) {
-    if(image->anim.getDone() || image->anim.getStart()){
-      count++;
+namespace UiUtils{
+
+  bool areStill(std::vector<MonoImage*> images){
+    int count = 0;
+    for (MonoImage* image : images) {
+      if(image->anim.getDone() || image->anim.getStart()){
+        count++;
+      }
     }
+      return (count == images.size()) ? true : false;
   }
-  return (count == images.size()) ? true : false;
+
+  bool isPointInElement(Coordinates point, UIElement* element){
+    Coordinates element_pos = element->getPos();
+    if ((point.x >= element_pos.x && point.x <= element_pos.x + element->getWidth()) && (point.y >= element_pos.y && point.y <= element_pos.y + element->getHeight())){
+      return true;
+    }
+    return false;
+  }
+
+  Coordinates centerPos(int x_pos, int y_pos, unsigned int w, unsigned int h){
+      int new_x = floor((float)x_pos+((float)w/2));
+      int new_y = floor((float)y_pos+((float)h/2));
+      return Coordinates(new_x, new_y);
+  }
+
+  UIElement* isThereACollision(unsigned int direction, Scene* scene, UIElement* focused, unsigned int max_distance){
+
+    Coordinates center_point = (focused->centered) ? focused->getPos() 
+                                                   : UiUtils::centerPos(focused->getPos().x, focused->getPos().y, focused->getWidth(), focused->getHeight());
+    Scene temporaryScene = *scene;
+    temporaryScene.elements.erase(focused->getId().ele_id);
+    Coordinates new_point;
+    switch(direction){
+      case RIGHT:
+        for(int i = 0; i<=max_distance;i++)
+        {
+          new_point = Coordinates(center_point.x+i, center_point.y);
+
+          for(auto elem : temporaryScene.elements){
+            if(isPointInElement(new_point, elem.second)){
+              return elem.second;
+            }
+          }
+        }
+        break;
+      case LEFT:
+        for(int i = 0; i<=max_distance;i++)
+        {
+          new_point = Coordinates(center_point.x-i, center_point.y);
+
+          for(auto elem : temporaryScene.elements){
+            if(isPointInElement(new_point, elem.second)){
+              return elem.second;
+            }
+          }
+        }
+        break;
+      case UP:
+        for(int i = 0; i<=max_distance;i++)
+        {
+          new_point = Coordinates(center_point.x, center_point.y+i);
+
+          for(auto elem : temporaryScene.elements){
+            if(isPointInElement(new_point, elem.second)){
+              return elem.second;
+            }
+          }
+        }
+        break;
+      case DOWN:
+        for(int i = 0; i<=max_distance;i++)
+        {
+          new_point = Coordinates(center_point.x, center_point.y-i);
+
+          for(auto elem : temporaryScene.elements){
+            if(isPointInElement(new_point, elem.second)){
+              return elem.second;
+            }
+          }
+        }
+        break;
+    }
+  return nullptr;
+  }
+
 }
 
+class UI{
+  public:
+  Focus focus;
+  std::unordered_map<unsigned int, Scene*> scenes;
+  UIElement* focusedElement = nullptr;
+  Scene* focusedScene = nullptr;
+  unsigned int max_focusing_distance = 64;
+
+  UI(Identity first_focus, Scene* first_scene){
+    addScene(first_scene);
+    focusScene(first_scene);
+    focusedElement = first_scene->elements[first_focus.ele_id];
+    focus = Focus(first_scene->id, focusedElement->getId().ele_id);
+  }
+
+  void addScene(Scene* scene){
+    scenes.insert({scene->id, scene});
+  }
+  void focusScene(Scene* scene){
+    focusedScene = scene;
+  }
+  void render(){
+    focusedScene->renderScene();
+  }
+  void focusDirection(unsigned int direction){
+    UIElement* next_element = UiUtils::isThereACollision(direction, focusedScene, focusedElement, max_focusing_distance);
+    if(next_element){
+        focus.focus(next_element->getId().scene_id, next_element->getId().ele_id);
+        focusedElement = next_element;
+    }
+  }
+};
 
 /**************************************************************************/
 /*!
--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,15 @@ Image8 smallGallery(HOME_SMALL_GALLERY_SIZE, HOME_SMALL_GALLERY_SIZE, home_small
 Image8 largeSettings(HOME_LARGE_SETTINGS_SIZE, HOME_LARGE_SETTINGS_SIZE, home_large_settings, 0xffff);
 Image8 smallSettings(HOME_SMALL_SETTINGS_SIZE, HOME_SMALL_SETTINGS_SIZE, home_small_settings, 0xffff);
 
-MonoImage play(&playTest, 64, 32);
-MonoImage settings(&largeSettings, 25, 32);
-MonoImage gallery(&largeGallery, 103, 32);
+MonoImage play(&playTest, 64, 32, 1);
+MonoImage settings(&largeSettings, 25, 32, 0);
+MonoImage gallery(&largeGallery, 103, 32, 2);
 
 std::vector<MonoImage*> apps = {&play, &settings, &gallery};
+std::vector<UIElement*> elements = {&play, &settings, &gallery};
+Scene home(elements, 0, true);
+Identity first_focus(0,1);
+UI ui(first_focus, &home);
 
 Focus focus(0,1);
 void setup() {
@@ -31,10 +35,6 @@ void setup() {
   tft.initR(INITR_GREENTAB);
   tft.setSPISpeed(78000000); //Absolute fastest speed tested, errors at 80000000
   tft.fillScreen(ST7735_BLACK);
-
-  play.setIdentity(0, 1);
-  gallery.setIdentity(0, 2);
-  settings.setIdentity(0, 0);
 
   play.setImg(&smallPlayTest);
   settings.setImg(&smallSettings);
@@ -47,10 +47,6 @@ void setup() {
   play.InitAnim(1,1.44,75);
   settings.InitAnim(1,1.44,75);
   gallery.InitAnim(1,1.44,75);
-
-  play.anim.stop();
-  settings.anim.stop();
-  gallery.anim.stop();
 }
 
 //-------------BEFORE LOOP----------------//
@@ -74,8 +70,8 @@ void framerate(bool render){
 }
 
 void handleAppSelectionAnimation(MonoImage* app, Image8* unfocused, Image8* focused, bool isPrimary = false){
-  if(focus.hasChanged() || (focus.isFirstBoot && isPrimary)){
-      if(focus.isFocusing(app->getId())){
+  if(ui.focus.hasChanged() || (ui.focus.isFirstBoot && isPrimary)){
+      if(ui.focus.isFocusing(app->getId())){
         if(app->getImg()==unfocused && app->anim.getStart())
         {
           app->overrideScaling = false;
@@ -93,7 +89,7 @@ void handleAppSelectionAnimation(MonoImage* app, Image8* unfocused, Image8* focu
     }
     
     if(app->anim.getDone()&&app->getImg()==unfocused){
-      if(focus.isFocusing(app->getId())){
+      if(ui.focus.isFocusing(app->getId())){
         app->overrideScaling = true;
         app->setImg(focused);
         app->setScale(1);
@@ -107,30 +103,41 @@ void handleAppSelectionAnimation(MonoImage* app, Image8* unfocused, Image8* focu
     }
 }
 
+
+
+
 void loop() {
     canvas.fillScreen(0x0000); //Fill the background with a black frame
 
     updateButtons(buttons);  //Update button states for every button
-    play.render();
-    settings.render();
-    gallery.render();
+    //home.renderScene();
+    ui.render();
 
-    if (button1.clickedOnce && !button2.clickedOnce && areStill(apps)) {
+    /*
+    if (button1.clickedOnce && !button2.clickedOnce && UiUtils::areStill(apps)) {
       if(focus.current.ele_id == 2)
       {focus.focus(0, 0);}
       else
       {focus.focus(0,focus.current.ele_id+1);}
     }
-    if (button2.clickedOnce&& !button1.clickedOnce && areStill(apps)) {
+    if (button2.clickedOnce&& !button1.clickedOnce && UiUtils::areStill(apps)) {
       if(focus.current.ele_id == 0)
       {focus.focus(0, 2);}
       else
       {focus.focus(0,focus.current.ele_id-1);}
+    }*/
+
+    if (button1.clickedOnce && !button2.clickedOnce && UiUtils::areStill(apps)) {
+      ui.focusDirection(RIGHT);
+    }
+    if (button2.clickedOnce&& !button1.clickedOnce && UiUtils::areStill(apps)) {
+      ui.focusDirection(LEFT);
     }
 
     handleAppSelectionAnimation(&play, &smallPlayTest, &playTest, true);
     handleAppSelectionAnimation(&settings, &smallSettings, &largeSettings);
     handleAppSelectionAnimation(&gallery, &smallGallery, &largeGallery);
+
 
     framerate(render_frametime);  //Render the framerate in the bottom-left corner on top of everything
     fastRender(0,0,canvas.getBuffer(),SCREENWIDTH,SCREENHEIGHT); //RENDER THE FRAME
